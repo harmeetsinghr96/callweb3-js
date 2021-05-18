b34113445daacae0d2024bc769e0b3371c253f32
harmeetsinghr96 <harmeetsinghr96@gmail.com>
2021-05-18 22:49:05 +0530

    added abi

--- src/initWeb3.js
+++ src/initWeb3.js
@@ -1,17 +1,48 @@
-const Web3 = require('web3');
+const Web3 = require("web3");
+const fs = require("fs");
+const path = require('path');
 
 class InitWeb3 {
+  constructor(
+    _infura,
+    _contractAddress,
+    _mainContractABI,
+    _tokenContractAddress,
+    _tokenContractABI
+  ) {
 
-    constructor(_infura, _contractAddress, _mainContractABI, _tokenContractAddress, _tokenContractABI) {
+    this.web3Instance = new Web3(_infura);
+    this.mainContractObj = new this.web3Instance.eth.Contract(
+      _mainContractABI,
+      _contractAddress
+    );
 
-        this.web3Instance = new Web3(_infura);
-        this.mainContractObj = new this.web3Instance.eth.Contract(_mainContractABI, _contractAddress);
+    if (_tokenContractAddress) {
+      this.tokenContractObj = new this.web3Instance.eth.Contract(
+        _tokenContractABI,
+        _tokenContractAddress
+      );
+    }
 
-        if (_tokenContractAddress) {
-            this.tokenContractObj = new this.web3Instance.eth.Contract(_tokenContractABI, _tokenContractAddress);
-        }
+    /*
+     * Writing JSON files (ABIS) 
+     */
+    this.writeJSONfile(_mainContractABI, _tokenContractABI);
+  }
 
-   }    
+  writeJSONfile(_mainContractABI, _tokenContractABI) {
+    if (_tokenContractABI) {
+      const p = path.resolve(__dirname, "abis", "tokenContractABI.json");
+      fs.readFile(p, "utf8", (err, data) => {
+        fs.writeFile(p, JSON.stringify(_tokenContractABI), "utf8", (err, data) => {});
+      });
+    }
+
+    const p = path.resolve(__dirname, "abis", "contractABI.json");
+    fs.readFile(p, "utf8", (err, data) => {
+      fs.writeFile(p, JSON.stringify(_mainContractABI), "utf8", (err, data) => {});
+    });
+  }
 }
 
 module.exports = InitWeb3;
